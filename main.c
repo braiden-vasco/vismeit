3f7678d1e6f0fc37ef754d8275cf12c4b0ae3e8c
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-09-21 14:35:04 +0000

    Create window

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <GLFW/glfw3.h>
+
+int main()
+{
+  if (!glfwInit())
+  {
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  GLFWwindow *const window = glfwCreateWindow(640, 480, "TITLE", NULL, NULL);
+
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  glViewport(0, 0, 640, 480);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glClearColor(0.2, 0.3, 0.3, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
+
+  return 0;
+}
