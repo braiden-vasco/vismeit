2aaa8e1465b749948fe04fb53f1c7663fce72dba
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-09-25 12:28:37 +0000

    Enable transparency

--- main.c
+++ main.c
@@ -90,6 +90,9 @@ void free_resources()
 
 void render()
 {
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
   glClearColor(1.0, 1.0, 1.0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT);
 
