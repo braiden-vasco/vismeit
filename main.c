c5e3c9d45565a0df219a72dedee6e529058d8496
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2018-09-25 09:50:32 +0000

    Add Ruby class Vismeit::Shader

--- main.c
+++ main.c
@@ -4,6 +4,21 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
+typedef struct {
+  GLuint gl_id;
+} rb_mVismeit_cShader_CDATA;
+
+static void Init_vismeit();
+
+static VALUE rb_mVismeit;
+static VALUE rb_mVismeit_cShader;
+
+static VALUE rb_mVismeit_cShader_alloc(VALUE rb_klass);
+static void  rb_mVismeit_cShader_free(rb_mVismeit_cShader_CDATA *free_cdata);
+
+static VALUE rb_mVismeit_cShader_initialize(VALUE rb_self,
+                                            VALUE rb_type, VALUE rb_source);
+
 static const char *const vs_source =
 #ifdef GL_ES_VERSION_2_0
   "#version 100                             \n"  // OpenGL ES 2.0
@@ -45,6 +60,8 @@ static void free_resources();
 int main(int argc, char* argv[]) {
   ruby_init();
 
+  Init_vismeit();
+
   glutInit(&argc, argv);
   glutInitContextVersion(2, 0);
   glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
@@ -70,6 +87,29 @@ int main(int argc, char* argv[]) {
 
 int init_resources()
 {
+  const VALUE vertex_shader = rb_eval_string(
+    "Vismeit::Shader.new(                                 \n"
+    "  :vertex,                                           \n"
+    "  \"#version 120                             \\n\" \\\n"
+    "  \"attribute vec2 coord2d;                  \\n\" \\\n"
+    "  \"void main(void) {                        \\n\" \\\n"
+    "  \"  gl_Position = vec4(coord2d, 0.0, 1.0); \\n\" \\\n"
+    "  \"}                                        \\n\",  \n"
+    ")                                                    \n"
+  );
+
+  const VALUE fragment_shader = rb_eval_string(
+    "Vismeit::Shader.new(                                 \n"
+    "  :fragment,                                         \n"
+    "  \"#version 120                             \\n\" \\\n"
+    "  \"void main(void) {                        \\n\" \\\n"
+    "  \"  gl_FragColor[0] = 0.0;                 \\n\" \\\n"
+    "  \"  gl_FragColor[1] = 0.0;                 \\n\" \\\n"
+    "  \"  gl_FragColor[2] = 1.0;                 \\n\" \\\n"
+    "  \"}                                        \\n\" \\\n"
+    ")                                                    \n"
+  );
+
   GLint compile_ok = GL_FALSE, link_ok = GL_FALSE;
 
   const GLuint vs = glCreateShader(GL_VERTEX_SHADER);
@@ -160,4 +200,88 @@ void onDisplay()
 
   glDisableVertexAttribArray(attribute_coord2d);
   glutSwapBuffers();
+}
+
+void Init_vismeit()
+{
+  rb_mVismeit = rb_define_module("Vismeit");
+
+  rb_mVismeit_cShader =
+    rb_define_class_under(rb_mVismeit, "Shader", rb_cObject);
+
+  rb_define_alloc_func(rb_mVismeit_cShader, rb_mVismeit_cShader_alloc);
+
+  rb_define_method(rb_mVismeit_cShader, "initialize",
+                   rb_mVismeit_cShader_initialize, 2);
+}
+
+VALUE rb_mVismeit_cShader_alloc(const VALUE rb_klass)
+{
+  rb_mVismeit_cShader_CDATA *const alloc_cdata =
+    ALLOC(rb_mVismeit_cShader_CDATA);
+
+  memset(alloc_cdata, 0, sizeof(rb_mVismeit_cShader_CDATA));
+
+  const VALUE result = Data_Wrap_Struct(rb_klass, NULL,
+                                        rb_mVismeit_cShader_free, alloc_cdata);
+
+  return result;
+}
+
+void rb_mVismeit_cShader_free(rb_mVismeit_cShader_CDATA *const free_cdata)
+{
+  glDeleteShader(free_cdata->gl_id);
+  free(free_cdata);
+}
+
+VALUE rb_mVismeit_cShader_initialize(
+  const VALUE rb_self,
+  const VALUE rb_type,
+  const VALUE rb_source
+)
+{
+  Check_Type(rb_source, T_STRING);
+
+  GLenum gl_shader_type;
+
+  if (rb_funcall(rb_type, rb_intern("=="), 1, ID2SYM(rb_intern("vertex"))))
+  {
+    gl_shader_type = GL_VERTEX_SHADER;
+  }
+  else if (rb_funcall(rb_type, rb_intern("=="), 1,
+                      ID2SYM(rb_intern("fragment"))))
+  {
+    gl_shader_type = GL_FRAGMENT_SHADER;
+  }
+  else
+  {
+    rb_raise(rb_eRuntimeError, "invalid type");
+  }
+
+  rb_mVismeit_cShader_CDATA *self_cdata;
+  Data_Get_Struct(rb_self, rb_mVismeit_cShader_CDATA, self_cdata);
+
+  self_cdata->gl_id = glCreateShader(gl_shader_type);
+
+  if (self_cdata->gl_id == 0)
+  {
+    rb_raise(rb_eRuntimeError, "can not generate shader object");
+  }
+
+  const GLchar *const gl_source_text = RSTRING_PTR(rb_source);
+  const GLint const gl_source_length = RSTRING_LEN(rb_source);
+
+  glShaderSource(self_cdata->gl_id, 1, &gl_source_text, &gl_source_length);
+
+  glCompileShader(self_cdata->gl_id);
+
+  GLint gl_compile_status;
+  glGetShaderiv(self_cdata->gl_id, GL_COMPILE_STATUS, &gl_compile_status);
+
+  if (gl_compile_status == 0)
+  {
+    rb_raise(rb_eRuntimeError, "can not compile shader");
+  }
+
+  return rb_self;
 }
